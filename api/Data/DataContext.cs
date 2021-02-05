3607ab7da9609e15020ce3eff02c64341bcb4c0e
Ravi Yadav <Ravi.Yadav@avl.com>
2021-02-05 13:06:38 +0530

    End of Section 2

---
+++ api/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using api.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
---
+++ api/Entity/AppUser.cs
@@ -0,0 +1,8 @@
+namespace api.Entity
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
